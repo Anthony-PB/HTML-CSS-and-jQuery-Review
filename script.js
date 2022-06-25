011c2ef85d6cf1595fc640a9bec74a568e43aa2e
Anthony Paredes-Bautista <105753408+Anthony-PB@users.noreply.github.com>
2022-06-25 12:28:31 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".stylep1").click(function(){
+	$(".stylep1").text("Anthony .P was here");
+});
+
+$(".styleme").dblclick(function(){
+    $(".stylep1").css("border","double","20px","red");
+    $(".stylep1").css("font-size","40px");
+    $(".stylep2").css("background-color","lime");
+    $(".stylep2").css("font-size","40px");
+    
+});
